8b2157fcd6a78021ae0b606eb79f4f77e9f86822
Niklas Mårdby <niklas.mardby@gmail.com>
2024-12-09 09:52:00 +0100

    Added type assertion demo stuff

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
 import "./style.css";
+
+const handleClick = (event: MouseEvent): void => {
+	console.log(
+		`Mouse clicked at (${event.clientX}, ${event.clientY}) coordinates.`
+	);
+};
+
+// const x: number;
+
+// const greetingsButton = <HTMLButtonElement>document.querySelector(".greeting");
+
+const greetingsButton = document.querySelector(
+	".greeting"
+) as HTMLButtonElement;
+
+//if (greetingsButton)
+//	greetingsButton.addEventListener("click", handleClick as EventListener);
+
+greetingsButton.addEventListener("click", handleClick);
+
+// type assertions as eller <>, vi föredrar as
