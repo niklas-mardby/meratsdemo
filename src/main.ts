397d7b971307e3a358f40d8022bff717ff09dd2d
Niklas Mårdby <niklas.mardby@gmail.com>
2024-12-09 10:33:46 +0100

    Added second demo

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./style.css";
 
+// ====== DEL 1 ===========================================
+
 const handleClick = (event: MouseEvent): void => {
 	console.log(
 		`Mouse clicked at (${event.clientX}, ${event.clientY}) coordinates.`
@@ -11,7 +13,7 @@ const handleClick = (event: MouseEvent): void => {
 // const greetingsButton = <HTMLButtonElement>document.querySelector(".greeting");
 
 const greetingsButton = document.querySelector(
-	".greeting"
+	"#greeting"
 ) as HTMLButtonElement;
 
 //if (greetingsButton)
@@ -20,3 +22,18 @@ const greetingsButton = document.querySelector(
 greetingsButton.addEventListener("click", handleClick);
 
 // type assertions as eller <>, vi föredrar as
+
+// ====== DEL 2 ===========================================
+
+// id fruit
+
+type InputEventHandler = (event: InputEvent) => void;
+
+const handleTextInput: InputEventHandler = (event) => {
+	const inputField = event.target as HTMLInputElement;
+	console.log(`Value from input field: ${inputField.value}`);
+};
+
+const textInput = document.querySelector("#fruit") as HTMLInputElement;
+
+textInput.addEventListener("input", handleTextInput as EventListener);
